2d8fcf413341127a63cdecf5565c709444967bbe
LihleMfundisi <101568276+LihleMfundisi@users.noreply.github.com>
2026-03-30 18:45:54 +0200

    Create loan_data table and import loan data
    Create loan_data table and bulk insert data from CSV

--- SQL/01_create_table.sql
+++ SQL/01_create_table.sql
@@ -1,1 +1,20 @@
+--Create table in the database to import data into
+CREATE TABLE loan_data (
+    gender TEXT,
+    married TEXT,
+    education TEXT,
+    self_employed TEXT,
+    property_area TEXT,
+    dependents TEXT,
+    applicant_income INT,
+    coapplicant_income INT,
+    loan_amount INT,
+    loan_term INT,
+    credit_history INT
+);
 
+--Bulk insert data into
+COPY loan_applications
+FROM 'C:\Users\mfund\Downloads\Loan Approval Prediciton data.csv'
+DELIMITER ','
+CSV HEADER;
